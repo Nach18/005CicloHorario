d12d79a9aea95b0cd1d8996fc61b788a45247451
Nach18 <ignaciolopezpuerto@gmail.com>
2017-12-06 11:54:46 +0100

    Hago un bucle donde el usuario indique una hora y el programa devuelva una respuesta

--- src/com/cice/ejercicios/Main.java
+++ src/com/cice/ejercicios/Main.java
@@ -22,18 +22,36 @@ public class Main {
 
         */
 
-        System.out.println("¿Que hora es compañero?");
+        int x=0;
 
-        int horaSC = SC.nextInt();
+        switch (x){
+            case 0:
+                System.out.println("X vale 0");
 
-        if (horaSC >= 6 && horaSC < 12){
-            System.out.println("Buenos Días");
+            case 1:
+                System.out.println("X vale 1");
+                break;
+            case 2:
+                System.out.println("X vale 2");
+        }
 
-        }else if (horaSC >= 12 && horaSC < 21) {
-            System.out.println("Buenas Tardes");
-        }else if (horaSC >= 21 && horaSC < 6) {
-            System.out.println("Buenas Tardes");
-        }else if (horaSC >= 19 && horaSC < 6);
-            System.out.println("Buenas Noches");
+
+        for (int i = 0; i<=24; i++) {
+
+            System.out.print("¿Que hora es compañero? (24H enteras)");
+
+            int horaSC = SC.nextInt();
+
+            if (horaSC >= 6 && horaSC < 12) {
+                System.out.println("Son las " + horaSC + ", buenos días");
+            } else if (horaSC >= 12 && horaSC < 21) {
+                System.out.println("Son las " + horaSC + ", buenas tardes");
+            } else if (horaSC >= 21 && horaSC <= 23 || horaSC >= 0 && horaSC < 6) {
+                System.out.println("Son las " + horaSC + ", buenas noches");
+            } else {
+                System.out.println("No te he entendido, ¿me puedes repetir la hora según el formato de 24 Horas?");
+            }
+
+        }
     }
 }
--- src/com/cice/ejercicios/Tablas.java
+++ src/com/cice/ejercicios/Tablas.java
@@ -4,13 +4,15 @@ public class Tablas {
 
     public static void main(String[] args) {
 
-
-        for(int i=1; i<=1000000000; i++) {
+        /*
+        for(int i=1; i<=100; i++) {
 
             System.out.println("2 x " + i + " = " + i*2);
             System.out.println("===========");
 
             int numero1;
         }
+        */
     }
+
 }
